10e85feaa737c3ec8dea9904f0ad8ff412a8b909
Scott Doxey <neogeek0101@gmail.com>
2024-11-29 20:57:33 -0500

    Added new http status code test.

--- src/http-status-codes.test.ts
+++ src/http-status-codes.test.ts
@@ -2,8 +2,29 @@ import { expect } from 'jsr:@std/expect';
 
 import { httpStatusCodes, httpStatusMessages } from './http-status-codes.ts';
 
-Deno.test('check http status codes have both string and number keys', () => {
-  expect(Object.keys(httpStatusMessages).length).toBe(
-    Object.keys(httpStatusCodes).length
-  );
-});
+Deno.test(
+  'check http status codes have similar counts for string and number keys',
+  () => {
+    expect(Object.keys(httpStatusMessages).length).toBe(
+      Object.keys(httpStatusCodes).length
+    );
+  }
+);
+
+Deno.test(
+  'check http status codes have the same string and number keys',
+  () => {
+    const codeValues = Array.from(new Set(Object.values(httpStatusCodes)));
+
+    const messageValues = Array.from(
+      new Set(Object.keys(httpStatusMessages).map(parseFloat))
+    );
+
+    const diff = Array.from(new Set(codeValues)).filter(
+      (value) => !messageValues.includes(value)
+    );
+
+    expect(codeValues.length).toBe(messageValues.length);
+    expect(diff.length).toBe(0);
+  }
+);
